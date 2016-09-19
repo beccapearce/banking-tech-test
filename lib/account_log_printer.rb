2e5ce5678ab7ff32445347157adfc382161556dc
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-19 15:50:38 +0100

    updates log printer to have headers

--- lib/account_log_printer.rb
+++ lib/account_log_printer.rb
@@ -2,8 +2,9 @@
 class AccountLogPrinter
 
   def print(log)
+    puts "date || credit || debit || balance"
     log.each do |transaction|
-      puts "#{transaction[:date]}, #{transaction[:credit]}, #{transaction[:debit]}, #{transaction[:balance]}"
+      puts "#{transaction[:date]} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]}"
     end
   end
 end
--- spec/account_log_printer_spec.rb
+++ spec/account_log_printer_spec.rb
@@ -9,6 +9,6 @@ describe AccountLogPrinter do
   it "prints out the account history" do
     account_log = double("account_log")
     allow(account_log).to receive(:transactions).and_return [{date: today, credit: 1000, balance: 1000}, {date: today, debit: 100, balance: 900}]
-    expect { subject.print(account_log.transactions) }.to output("2016-09-19, 1000, , 1000\n2016-09-19, , 100, 900\n").to_stdout
+    expect { subject.print(account_log.transactions) }.to output("date || credit || debit || balance\n2016-09-19 || 1000 ||  || 1000\n2016-09-19 ||  || 100 || 900\n").to_stdout
   end
 end
